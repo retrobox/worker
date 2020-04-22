5b10a78ff91b432167cfa2b3d45792ca9e7eb011
Matthieu Bessat <spamfree@matthieubessat.fr>
2020-04-22 16:19:08 +0200

    fix: add toString on stdout in warper.js

--- warper.js
+++ warper.js
@@ -1,10 +1,10 @@
+console.log('Starting worker with node.js...')
+
 const spawn = require('child_process').spawn;
 const ls = spawn('php', ['worker.php']);
 
-console.log('Worker with node.js')
-
 ls.stdout.on('data', (data) => {
- console.log(data);
+ console.log(data.toString());
 });
 
 ls.stderr.on('data', (data) => {
